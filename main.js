d1df2c17bc53ebe1ab42a85aba896caa519f3036
Karishma <kdaya001@gmail.com>
2021-10-31 22:51:36 +1100

    add wheel of fortune logic

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var wordLetters = ['F', 'O', 'X'];
 var guessedLetters = [];
+var reward = 0;
 
 function guessLetter(guessedLetter) {
     console.log(guessedLetters);
+    let thisReward = Math.floor(Math.random() * 100 + 1);
+    let inWord = false;
 
     for (let i = 0; i < wordLetters.length; i++) {
         if (guessedLetter === wordLetters[i]) {
@@ -10,8 +13,12 @@ function guessLetter(guessedLetter) {
             console.log(
                 `congratulations, you guessed a letter.\n ${guessedLetters}`
             );
+            inWord = true;
         }
     }
+    
+    if(inWord) reward += thisReward;
+    else reward -= thisReward;
 }
 
 function setup(wordLetters) {
@@ -26,3 +33,5 @@ while (guessedLetters.includes(`-`)) {
     let guess = prompt('Guess a letter');
     guessLetter(guess);
 }
+
+console.log(`You guessed the word: ${wordLetters} and got $${reward}`);
